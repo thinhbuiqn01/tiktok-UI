5e3431eb35641c36beede47c13691d0df1287a9c
thinhbui01 <buithinh.qng,01@gmail.com>
2022-07-23 00:21:34 +0700

    Part2: create header nav search logo for project

---
+++ src/asset/images/index.js
@@ -0,0 +1,4 @@
+const images = {
+    logo: require('~/asset/images/logo.svg').default,
+};
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,37 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import images from '~/asset/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            {/* logo */}
-            {/* search */}
-        </div>
-    </header>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+
+                <div className={cx('search')}>
+                    <input spellCheck="false" placeholder="Search videos or accounts" />
+
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
